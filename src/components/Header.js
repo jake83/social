a908bc16db7e2c54ddb172fabe2a8917504c7d43
jake83 <jwatson1983@gmail.com>
2022-07-25 15:40:51 +0100

    Starting to add in links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,50 @@
 import React from "react";
+import { connect } from "react-redux";
 import Link from "./Link";
 import GoogleAuth from "./GoogleAuth";
 
-const Header = () => {
-  return (
-    <div className="ui inverted menu">
-      <Link href="/" className="item">
-        <i className="home icon" />
-      </Link>
-      <Link href="/youtube" className="item">
-        <i className="youtube icon" />
-        <span>YouTube</span>
-      </Link>
-      <Link href="/instagram" className="item">
-        <i className="instagram icon" />
-        <span>Instagram</span>
-      </Link>
-      <Link href="/twitter" className="item">
-        <i className="twitter icon" />
-        <span>Twitter</span>
-      </Link>
-      <div className="right menu">
+class Header extends React.Component {
+  renderProfileButton() {
+    if (this.props.isSignedIn) {
+      return (
         <Link href="/profile" className="item">
           <i className="user icon" />
         </Link>
-        <GoogleAuth />
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="ui inverted menu">
+        <Link href="/" className="item">
+          <i className="home icon" />
+        </Link>
+        <Link href="/youtube" className="item">
+          <i className="youtube icon" />
+          <span>YouTube</span>
+        </Link>
+        <Link href="/instagram" className="item">
+          <i className="instagram icon" />
+          <span>Instagram</span>
+        </Link>
+        <Link href="/twitter" className="item">
+          <i className="twitter icon" />
+          <span>Twitter</span>
+        </Link>
+        <div className="right menu">
+          {this.renderProfileButton()}
+          <GoogleAuth />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
-export default Header;
+export default connect(mapStateToProps)(Header);
