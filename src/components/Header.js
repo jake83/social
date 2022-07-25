3054e18f4aa0b3dd384ad6ff46bafbdd404f7c05
jake83 <jwatson1983@gmail.com>
2022-07-25 13:18:08 +0100

    Starting to add in links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,22 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link href="/" className="item">
-        <i className="home icon"></i>
+        <i className="home icon" />
       </Link>
       <Link href="/youtube" className="item">
+        <i className="youtube icon" />
         YouTube
       </Link>
       <Link href="/instagram" className="item">
+        <i className="instagram icon" />
         Instagram
       </Link>
       <Link href="/twitter" className="item">
+        <i className="twitter icon" />
         Twitter
       </Link>
       <Link href="/profile" className="item">
-        <i className="user icon"></i>
+        <i className="user icon" />
       </Link>
     </div>
   );
