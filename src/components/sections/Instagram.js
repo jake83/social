061d135e679f2ce6cc967771de2aa3664169cffa
jake83 <jwatson1983@gmail.com>
2022-08-02 23:27:21 +0100

    Twitter functionality WIP

--- src/components/sections/Instagram.js
+++ src/components/sections/Instagram.js
@@ -13,6 +13,7 @@ class Instagram extends React.Component {
     if (!this.props.profile) {
       return <div>Loading...</div>;
     } else {
+      console.log(this.props.profile.instagram);
       return (
         <InstagramFeed token={this.props.profile.instagram} counter="12" />
       );
